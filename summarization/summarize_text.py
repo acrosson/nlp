d4ebaee65994a6224b6a808d9c793b6236713ecd
alexandercrosson <alexcrosson@gmail.com>
2016-06-13 16:31:38 -0700

    Added doc strings

--- summarization/summarize_text.py
+++ summarization/summarize_text.py
@@ -12,6 +12,11 @@ stop = stopwords.words('english')
 NOUNS = ['NN', 'NNS', 'NNP', 'NNPS']
 
 def clean_document(document):
+    """Cleans document by removing unnecessary punctuation. It also removes
+    any extra periods and merges acronyms to prevent the tokenizer from
+    splitting a false sentence
+
+    """
     # Remove all characters outside of Alpha Numeric
     # and some punctuation
     document = re.sub('[^A-Za-z .-]+', ' ', document)
@@ -28,10 +33,17 @@ def clean_document(document):
     return document
 
 def remove_stop_words(document):
+    """Returns document without stop words"""
     document = ' '.join([i for i in document.split() if i not in stop])
     return document
 
 def similarity_score(t, s):
+    """Returns a similarity score for a given sentence.
+
+    similarity score = the total number of tokens in a sentence that exits
+                        within the title / total words in title
+
+    """
     t = remove_stop_words(t.lower())
     s = remove_stop_words(s.lower())
     t_tokens, s_tokens = t.split(), s.split()
@@ -40,6 +52,7 @@ def similarity_score(t, s):
     return score
 
 def merge_acronyms(s):
+    """Merges all acronyms in a given sentence. For example M.I.T -> MIT"""
     r = re.compile(r'(?:(?<=\.|\s)[A-Z]\.)+')
     acronyms = r.findall(s)
     for a in acronyms:
@@ -47,6 +60,18 @@ def merge_acronyms(s):
     return s
 
 def rank_sentences(doc, doc_matrix, feature_names, top_n=3):
+    """Returns top_n sentences. Theses sentences are then used as summary
+    of document.
+
+    input
+    ------------
+    doc : a document as type str
+    doc_matrix : a dense tf-idf matrix calculated with Scikit's TfidfTransformer
+    feature_names : a list of all features, the index is used to look up
+                    tf-idf scores in the doc_matrix
+    top_n : number of sentences to return
+    
+    """
     sents = nltk.sent_tokenize(doc)
     sentences = [nltk.word_tokenize(sent) for sent in sents]
     sentences = [[w for w in sent if nltk.pos_tag([w])[0][1] in NOUNS] for sent in sentences]
